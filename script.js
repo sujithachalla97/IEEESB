2bdfc4b4d7c554bf035e241f52dcf59f9064a9df
sujithachalla97 <168841679+sujithachalla97@users.noreply.github.com>
2024-07-06 10:22:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+        document.querySelector('.navbar').classList.toggle('active');
+    });
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+    
